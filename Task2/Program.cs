0a156804f86c93ba3979e65b4c8d46421f5e4abe
Igor Martynchik <sepsetto@gmail.com>
2023-06-11 01:35:43 +0500

    added task 4 6 8

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,3 +3,12 @@
 // a = 2, b = 10 -> max = 10
 // a = -9, b = -3 -> max = -3
 
+Console.WriteLine("Введите два числа: ");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2)
+    Console.WriteLine("max = " + number1);
+else {
+    Console.WriteLine("max = " + number2);
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int start = 2;
+
+if(number > 1)
+{
+    while(start <= number)
+    {
+        Console.Write(start + " ");
+        start = start + 2;
+    }
+}
